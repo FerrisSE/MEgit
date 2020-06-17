de9787c0849eff3baf2f05ee1b7a922fe832489a
Killer7502 <sawyerkole@gmail.com>
2020-06-17 12:08:30 -0400

    Added in a test value and a printline for it.

--- NewGame/src/Game.java
+++ NewGame/src/Game.java
@@ -3,7 +3,8 @@ public class Game {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int testValue = 0;
+		System.out.println("Test Value: " + testValue);
 	}
 
 }
